fa1685440338892a453d7f2f4c5a14b8d27d85fd
RADU ZAMFIR <work.radu.zamfir@gmail.com>
2023-01-02 15:31:19 +0200

    changed colors, js logic about hamburger menu btn

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,6 @@
 
 
 const menuBtn = document.querySelector('.menu-btn');
-const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
@@ -12,14 +11,15 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
   if(!showMenu) {
-    hamburger.classList.add('open');
+    menuBtn.classList.add('fa-xmark');
     nav.classList.add('open');
     menuNav.classList.add('open');
     navItems.forEach(item => item.classList.add('open'));
 
     showMenu = true;
   } else {
-    hamburger.classList.remove('open');
+    menuBtn.classList.add('fa-bars');
+    menuBtn.classList.remove('fa-xmark');
     nav.classList.remove('open');
     menuNav.classList.remove('open');
     navItems.forEach(item => item.classList.remove('open'));
